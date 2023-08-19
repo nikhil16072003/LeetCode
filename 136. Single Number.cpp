349831f4fcb5fd8c622aff2aa84791b60be795a4
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-08-19 08:13:10 +0530

    Create 136. Single Number.cpp

---
+++ 136. Single Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=1;i<n;i=i+2){
+            if(nums[i]!=nums[i-1]){
+                return nums[i-1];
+            }
+        }
+       return nums[n-1];
+    }
+};
