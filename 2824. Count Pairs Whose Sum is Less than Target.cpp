3fe6897ee5c36ea54db4340d7acc9f76773c6e8f
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-09-10 09:52:40 +0530

    Update 2824. Count Pairs Whose Sum is Less than Target.cpp

--- 2824. Count Pairs Whose Sum is Less than Target.cpp
+++ 2824. Count Pairs Whose Sum is Less than Target.cpp
@@ -1,3 +1,5 @@
+//its runs with 100% beats
+//Time complexity is O(n^2)
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
@@ -8,6 +10,27 @@ public:
             for(j=i+1;j<n;j++){
                 if(nums[i]+nums[j]<target){
                     count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+//////////////////another solution same as before solution but slight difference /////////////////////////////////////
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int n=nums.size();
+        int i,j;
+        int count=0;
+        sort(nums.begin(),nums.end());
+        for(i=0;i<n;i++){
+            for(j=i+1;j<n;j++){
+                if(nums[i]+nums[j]<target){
+                    count++;
+                }
+                else{
+                    break;
                 }
             }
         }
