7b0d8af5c56edb2248b0733d878a050057238f34
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-09-16 09:50:14 +0530

    Update 41. First Missing Positive.cpp

--- 41. First Missing Positive.cpp
+++ 41. First Missing Positive.cpp
@@ -1,57 +1,28 @@
-//my version code
-
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size()-1;
-        int i;
-        int last= nums[n];
-        int laste=nums[n]+1;
-        nums.push_back(laste);
-        int extra=nums[n+1];
-        
-        
-
-        int first= nums[0];
-
-        if(first>1){
-            return 1;
-        }
-        if(n+1==1 && nums[n]==1){
-            return 2;
-        }
-        for(i=first;i<=last;i++){
-            if(i<=0){
-                continue;
-            }
-            if(nums[i]!=i){
-                break;
-            }
-        }
-        return i;
+       set<int>a;
+       int i;
+       int n=nums.size();
+       for(i=0;i<n;i++){
+           if(nums[i]>0){
+               a.insert(nums[i]);
+           }
+       }
+       vector<int>ans;
+       for(auto i:a){
+           ans.push_back(i);
+       }
+       int n1=ans.size();
+       vector<int>temp;
+       for(i=1;i<=n1;i++){
+           temp.push_back(i);
+       }
+       for(i=0;i<n1;i++){
+           if(ans[i]!=temp[i]){
+               return temp[i];
+           }
+       }
+       return i+1;
     }
 };
-
-/////////////////////////////////////////////////////////////////////////
-class Solution {
-public:
-    int firstMissingPositive(vector<int>& nums) {
-        set<int> s(nums.begin(),nums.end());
-        int i=1;
-        int n=s.size();
-        while(i<=n)
-        {
-            if(s.find(i)!=s.end())
-            {
-                i++;
-            }
-            else
-            {
-                return i;
-            }
-        }
-        return n+1;
-    }
-};
-//////////////////////////////////////////////////////////////////////
