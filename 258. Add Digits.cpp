e65ca71ba4377532a8f12f79a9f8dcb87c77317a
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-09-17 20:15:41 +0530

    Update 258. Add Digits.cpp

--- 258. Add Digits.cpp
+++ 258. Add Digits.cpp
@@ -1,3 +1,27 @@
+//////
+/*
+The logic behind this approach , I think is related to the divisibility test of 9.
+If you don't know then , "To check if a number is divisible by 9, add the digits of 
+the number and check if the sum is divisible by 9 or not. If yes , is the case , then 
+the number is divisible by 9 , otherwise it's not ."Now if you notice if a number is 
+divisible by 9(and 3) , then if we follow the procedure as mention in the question 
+(sum of digits of every previous num) , we would always find the new_num(the sum of the digits )
+to be divisible by 9(and 3).
+
+*/
+/////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    int addDigits(int num) {
+      if(num==0)
+          return 0;
+      else if(num%9==0)
+          return 9;
+      else
+          return num%9;
+    }
+};
+//////////////////////////////////////////////////////////////////
 class Solution {
 public:
     int addDigits(int num) {
