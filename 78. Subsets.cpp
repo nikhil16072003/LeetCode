df33e3a4cd5fb035e28f9de5d2b8e5d18ea2129c
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-09-29 20:04:31 +0530

    Create 78. Subsets.cpp

---
+++ 78. Subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+void solve(vector<int>&nums,vector<vector<int>>&result,vector<int>&ans,int st,int end){
+    result.push_back(ans);
+    int i;
+    for(i=st;i<end;i++){
+        ans.push_back(nums[i]);
+        solve(nums,result,ans,i+1,end);
+        ans.pop_back();
+    }
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>result;
+        vector<int>ans;
+        int n=nums.size();
+        solve(nums,result,ans,0,n);
+        return result;
+        
+    }
+};
