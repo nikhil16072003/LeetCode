ea8dd11682f920fa0358253a553d28d794bb0a66
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-10-01 14:08:37 +0530

    Update 78. Subsets.cpp

--- 78. Subsets.cpp
+++ 78. Subsets.cpp
@@ -18,3 +18,29 @@ void solve(vector<int>&nums,vector<vector<int>>&result,vector<int>&ans,int st,in
         
     }
 };
+////////////////100% beats////////////////////////////////////////
+class Solution {
+public:
+void solve(vector<int>&nums,vector<vector<int>>&result,vector<int>&ans,int st,int end){
+    
+    
+    if(st>=end){
+        result.push_back(ans);
+        return;
+    }
+        solve(nums,result,ans,st+1,end);
+        ans.push_back(nums[st]);
+        solve(nums,result,ans,st+1,end);
+        ans.pop_back();
+    
+  
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>result;
+        vector<int>ans;
+        int n=nums.size();
+        solve(nums,result,ans,0,n);
+        return result;
+        
+    }
+};
