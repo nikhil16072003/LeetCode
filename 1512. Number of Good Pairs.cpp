1110f395ecbf0993c887f7c6c9eeea9d6317299a
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-10-03 09:13:08 +0530

    Create 1512. Number of Good Pairs.cpp

---
+++ 1512. Number of Good Pairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int i,j;
+        int n=nums.size();
+        int count=0;
+        for(i=0;i<n;i++){
+            for(j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
