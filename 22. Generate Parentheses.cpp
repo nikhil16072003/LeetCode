0d0a2f4d907c5bb5a3a1fd0d616ef10855f6f772
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-10-10 12:25:33 +0530

    Create 22. Generate Parentheses.cpp

---
+++ 22. Generate Parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    
+public:
+void solve(int cnt1,int cnt2,int n,vector<string>&ans,string &op,int ind){
+        if(ind>=n*2){
+            ans.push_back(op);
+            return;
+        }
+        if(cnt1<n && ind<2*n-1){
+            op=op+'(';
+            solve(cnt1+1,cnt2,n,ans,op,ind+1);
+            op.pop_back();
+        }
+        if(cnt2<cnt1 && ind>0){
+            op=op+')';
+            solve(cnt1,cnt2+1,n,ans,op,ind+1);
+            op.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        string op="";
+        vector<string>ans;
+        solve(0,0,n,ans,op,0);
+        return ans;
+        
+    }
+};
