f56f7e8e5cf576686ef18cb3a6715daec6691081
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-10-14 09:14:22 +0530

    Create 2206. Divide Array Into Equal Pairs.cpp

---
+++ 2206. Divide Array Into Equal Pairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        int n=nums.size();
+        if(n%2==1){
+            return false;
+        }
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        for(auto i:mp){
+            if(i.second%2!=0){
+                return false;
+            }
+           
+        }
+        return true;
+        
+    }
+};
